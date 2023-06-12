0f019ed8674c0791e2a085bd01f8d67b3333cd61
HitBlast <hitblastlive@gmail.com>
2023-06-12 22:14:48 +0600

    add formula

---
+++ Formula/mcsrvstat.rb
@@ -0,0 +1,15 @@
+class Mcsrvstat < Formula
+  desc "A hybrid and asynchronous Nim wrapper for the Minecraft Server Status API."
+  homepage "https://github.com/hitblast/mcsrvstat.nim"
+  url "https://github.com/hitblast/mcsrvstat.nim/archive/refs/tags/1.1.0.tar.gz"
+  sha256 "191959bee40a83d62d89c4d4f684343f5726b613a2bcf6e31ecfc4fcec13e294"
+  license "MIT"
+
+  depends_on "openssl"
+  depends_on "nim" => :build
+
+  def install
+    system "nimble", "-d:release", "build", "--accept"
+    bin.install "mcsrvstat"
+  end
+end
