5faf8289a72cd919abd23eb9b57827fbc4a692a3
HitBlast <hitblastlive@gmail.com>
2023-06-17 12:13:18 +0600

    updated formula for mcsrvstat

--- Formula/mcsrvstat.rb
+++ Formula/mcsrvstat.rb
@@ -1,8 +1,8 @@
 class Mcsrvstat < Formula
   desc "A hybrid and asynchronous Nim wrapper for the Minecraft Server Status API."
   homepage "https://github.com/hitblast/mcsrvstat.nim"
-  url "https://github.com/hitblast/mcsrvstat.nim/archive/refs/tags/1.1.0.tar.gz"
-  sha256 "191959bee40a83d62d89c4d4f684343f5726b613a2bcf6e31ecfc4fcec13e294"
+  url "https://github.com/hitblast/mcsrvstat.nim/archive/refs/tags/1.2.0.tar.gz"
+  sha256 "517f1e80fa5f5c217d2a20631940da6879dc36156eeb906c48241fb56c7373c1"
   license "MIT"
 
   depends_on "openssl"
