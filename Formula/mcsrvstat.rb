1201b8131a26faa7d6bfe0ce3df6c2637e8f46f4
HitBlast <hitblastlive@gmail.com>
2024-03-18 23:49:02 +0600

    bumped versions and checksums

--- Formula/mcsrvstat.rb
+++ Formula/mcsrvstat.rb
@@ -1,8 +1,8 @@
 class Mcsrvstat < Formula
   desc "A hybrid and asynchronous Nim wrapper for the Minecraft Server Status API."
   homepage "https://github.com/hitblast/mcsrvstat.nim"
-  url "https://github.com/hitblast/mcsrvstat.nim/archive/refs/tags/1.3.1.tar.gz"
-  sha256 "e9680f9dbf140a07f90d2f7b1c97c03bba4addc648bcd66c1b7677ab14213fce"
+  url "https://github.com/hitblast/mcsrvstat.nim/archive/refs/tags/1.3.2.tar.gz"
+  sha256 "a9040de5b994a68db3c062716d995781d90f798a7c330fcaf4156302c49b6588"
   license "MIT"
 
   depends_on "openssl"
--- Formula/nimip.rb
+++ Formula/nimip.rb
@@ -1,8 +1,8 @@
 class Nimip < Formula
   desc "Asynchronously lookup IP addresses with this tiny, hybrid Nim application"
   homepage "https://github.com/hitblast/nimip"
-  url "https://github.com/hitblast/nimip/archive/refs/tags/1.2.0.tar.gz"
-  sha256 "4124e99f7837f10c34e931573cfc20a36bec55263cd37b000920f8eaebe14f5e"
+  url "https://github.com/hitblast/nimip/archive/refs/tags/1.2.1.tar.gz"
+  sha256 "d1b29c583274f5d8e52a683c9947b0554bcd5e831c960772ba466c9d2d2dab75"
   license "MIT"
 
   depends_on "openssl"
